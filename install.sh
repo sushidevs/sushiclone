3fab0e50d2aabd6e926c44bc3cac0c7763dd6373
SUSHIBOX <89353151+SUSHIB0X@users.noreply.github.com>
2021-08-27 20:11:23 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+if ! command -v systemctl >/dev/null 2>&1; then
+    echo "> Sorry but this scripts is only for Linux with systemd, eg: Ubuntu 16.04+/Centos 7+ ..."
+    exit 1
+fi
+
+if [[ $(id -u) -ne 0 ]]; then
+    echo "This script must be run as root" 
+    exit 1
+fi
+
+CLDBIN=/usr/bin/sclone
+OSARCH=$(uname -m)
+case $OSARCH in 
+    x86_64)
+        BINTAG=linux-amd64
+        ;;
+    i*86)
+        BINTAG=linux-386
+        ;;
+    arm64)
+        BINTAG=linux-arm64
+        ;;
+    arm*)
+        BINTAG=linux-arm
+        ;;
+    *)
+        echo "unsupported OSARCH: $OSARCH"
+        exit 1
+        ;;
+esac
+
+wget https://github.com/SUSHIB0X/sushiclone/raw/main/sushiclone.tar.gz
+tar xvzf sushiclone.tar.gz -C /usr/bin
+rm sushiclone.tar.gz
+chmod 0755 ${CLDBIN}
+
+sclone version
